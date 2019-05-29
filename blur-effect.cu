eb963cfcaa346e6a99aa8496ffdf0779d3346795
ronaldsg20 <roasarmientoga@unal.edu.co>
2019-05-29 16:02:54 -0500

    change

--- blur-effect.cu
+++ blur-effect.cu
@@ -60,11 +60,11 @@
      
     int tn = (blockDim.x * blockIdx.x) + threadIdx.x+1;
     
-    int ini = (int)((int)width/(int)totalThreads)*(tn);
-    int fin = (int)((int)width/(int)totalThreads)+ini;
+    int ini = (int)((int)*width/(int)*totalThreads)*(tn);
+    int fin = (int)((int)*width/(int)*totalThreads)+ini;
     for (int i = ini; i < fin; i++)
     {
-        for (int j = 0; j < (int)height; j++)
+        for (int j = 0; j < (int)*height; j++)
         {
             aplyBlur(i,j,*kernel, *width,*height,input, output);
         }
