20ccb766fd8bb77fbd8c997e9978689e2ad039ae
ronaldsg20 <roasarmientoga@unal.edu.co>
2019-06-02 01:06:50 -0500

    script

--- script_ejecutar_todo.sh
+++ script_ejecutar_todo.sh
@@ -8,7 +8,7 @@ echo "|=========================|" >> resultados.txt
 for kernel in 2 3 4 5 6 7 8 9 10 11 12 13 14;
 do
     echo "-------Kernel: $kernel -----" >> resultados.txt
-    { time ./blur-effect 720-image.jpg 720-image-out.jpg $kernel >/dev/null 2>&1;} |&  tee -a resultados.txt
+    { time ./blur-effect 720-image.jpg ./outputs/720-image-out-$kernel.jpg $kernel >/dev/null 2>&1;} |&  tee -a resultados.txt
 done
 
 
@@ -19,7 +19,7 @@ echo "|=========================|" >> resultados.txt
 for kernel in 2 3 4 5 6 7 8 9 10 11 12 13 14;
 do
     echo "-------Kernel: $kernel ------" >> resultados.txt
-    { time ./blur-effect 1080-image.jpeg 1080-image-out.jpeg $kernel>/dev/null 2>&1;} |&  tee -a resultados.txt
+    { time ./blur-effect 1080-image.jpeg ./outputs/1080-image-out-$kernel.jpeg $kernel>/dev/null 2>&1;} |&  tee -a resultados.txt
 done
 echo "|=========================|" >> resultados.txt
 echo "|--------Imagen 4K--------|" >> resultados.txt
@@ -28,6 +28,6 @@ echo "|=========================|" >> resultados.txt
 for kernel in 2 3 4 5 6 7 8 9 10 11 12 13 14;
 do
     echo "-------Kernel: $kernel ------" >> resultados.txt
-    { time ./blur-effect 4k-image.jpg 4k-image-out.jpg $kernel >/dev/null 2>&1;} |&  tee -a resultados.txt
+    { time ./blur-effect 4k-image.jpg ./outputs/4k-image-out.jpg-$kernel $kernel >/dev/null 2>&1;} |&  tee -a resultados.txt
 done
 
